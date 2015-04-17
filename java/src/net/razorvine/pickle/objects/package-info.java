9b8d1bee3461cf60c8103e692295cbee0fa69f55
Alex Sandro <geniofuturo@gmail.com>
2015-04-17 16:32:03 -0300

    Removendo todo conteúdo não relacionado ao projeto.

--- java/src/net/razorvine/pickle/objects/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Object constructors and other utility classes for the pickle package.
- * 
- * @author Irmen de Jong (irmen@razorvine.net)
- * @version 4.3
- * @see net.razorvine.pickle
- */
-package net.razorvine.pickle.objects;
