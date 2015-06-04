f3dc52653abe5b07e778c7d914c399b0d8a984a6
Alex Sandro <geniofuturo@gmail.com>
2015-06-04 20:12:39 -0300

    Reestruturando o script de construção do projeto.

--- builder.py
+++ builder.py
@@ -9,45 +9,63 @@ __author__ = 'alex'
 class Builder(object):
     """build release pack"""
 
-    def __init__(self, dirs, outdir, ftypes, location=os.getcwd(), dropout=False):
+    def __init__(self, dirs, outdir, ftypes,
+                 curdir=os.path.dirname(__file__),
+                 dropout=False):
         self.dirs = dirs
         self.ftypes = ftypes
-        self.location = os.path.abspath(os.path.normpath(location))
+        self.curdir = os.path.abspath(os.path.normpath(curdir))
         self.outdir = os.path.abspath(os.path.normpath(outdir))
         self.dropout = dropout
 
-    def build(self):
-        # remove o diretório build se configurado.
+    def _clean_if_needed(self):
         if self.dropout and os.path.exists(self.outdir):
             shutil.rmtree(self.outdir)
 
+    def process_filename(self, root, rel_dirname, filename):
+        name, ext = os.path.splitext(filename)
+        if ext in self.ftypes:
+            old_filepath = os.path.join(root, filename)
+            new_filepath = os.path.join(self.outdir, rel_dirname, filename)
+            if not os.path.exists(new_filepath):
+                shutil.copy(old_filepath, new_filepath)
+
+    def process_new_filesdir(self, rel_dirname, dirname):
+        new_dir = os.path.join(self.outdir, rel_dirname, dirname)
+        if not os.path.exists(new_dir):
+            os.makedirs(new_dir)
+
+    def get_old_new_dir(self, dirname):
+        old_dir = os.path.join(self.curdir, dirname)
+        new_dir = os.path.join(self.outdir, dirname)
+        return old_dir, new_dir
+
+    @staticmethod
+    def make_new_dirs(old_dir, new_dir):
+        if not os.path.exists(new_dir) and os.path.isdir(old_dir):
+            os.makedirs(new_dir)
+
+    def build(self):
+        # remove o diretório build se configurado.
+        self._clean_if_needed()
+
         for dirname in self.dirs:
-            old_location = os.path.join(self.location, dirname)
-            new_location = os.path.join(self.outdir, dirname)
+            old_dir, new_dir = self.get_old_new_dir(dirname)
 
-            if not os.path.exists(new_location) and os.path.isdir(old_location):
-                os.makedirs(new_location)
+            self.make_new_dirs(old_dir, new_dir)
 
-            for root, dirs, files in os.walk(old_location):
-                rel_dirname = root.replace(self.location, '').lstrip(os.sep)
+            for root, dirs, files in os.walk(old_dir):
+                rel_dirname = os.path.relpath(root, self.curdir)
 
                 for dname in dirs:
-                    _new_location = os.path.join(self.outdir, rel_dirname, dname)
-                    if not os.path.exists(_new_location):
-                        os.makedirs(_new_location)
+                    self.process_new_filesdir(rel_dirname, dname)
 
                 for filename in files:
-                    name, ext = os.path.splitext(filename)
-                    if ext in self.ftypes:
-                        old_filepath = os.path.join(root, filename)
-                        new_filepath = os.path.join(self.outdir, rel_dirname, filename)
-
-                        if not os.path.exists(new_filepath):
-                            shutil.copy(old_filepath, new_filepath)
+                    self.process_filename(root, rel_dirname, filename)
 
 
-def main(dirs, buildpath, ftypes, dropout):
-    builder = Builder(dirs, buildpath, ftypes, dropout=dropout)
+def main(*args, **kwargs):
+    builder = Builder(*args, **kwargs)
     builder.build()
 
 
@@ -68,5 +86,5 @@ if __name__ == '__main__':
         ['api-lua3.2', 'luabit', 'sha1'],
         args.buildpath,
         ['.lua'],
-        args.dropout
+        dropout = args.dropout
     )
