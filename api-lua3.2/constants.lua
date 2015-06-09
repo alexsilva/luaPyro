ee541a9bb6afe5a72c2dcfbc1f6bf0a02b45a0b3
Alex Sandro <geniofuturo@gmail.com>
2015-06-09 19:12:28 -0300

    Adicionando a versão da biblioteca que obedecerá a uma tag no controle de versão.

--- api-lua3.2/constants.lua
+++ api-lua3.2/constants.lua
@@ -6,23 +6,19 @@
 -- To change this template use File | Settings | File Templates.
 --
 -- Definitions of various hard coded constants.
-
 -- Pyro - Python Remote Objects.  Copyright by Irmen de Jong (irmen@razorvine.net).
-
 -- Pyro version
 
 constants = {
     VERSION = "4.31",
-
     -- standard object name for the Daemon object
     DAEMON_NAME = "Pyro.Daemon",
-
     -- standard name for the Name server itself
     NAMESERVER_NAME = "Pyro.NameServer",
-
     -- standard name for Flame server
     FLAME_NAME = "Pyro.Flame",
-
     -- wire protocol version. Note that if this gets updated, Pyrolite might need an update too.
     PROTOCOL_VERSION = 47,
+    -- version of the client
+    LIBRABRY_VERSION = '1.0-dev'
 }
